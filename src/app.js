5461c938b2c8f9bd91861356e3a09113b3fb1b8a
Nitin Kumar <122643736+nitin1280@users.noreply.github.com>
2023-07-22 22:33:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
-const express = require("express");
-const fs = require("fs");
+const fs = require('fs');
+const express = require('express');
 const app = express();
+//Aim: With the help of router, get all the product with router.GET request and create a product with router.POST request
 
-// Importing the userRoutes module from a file in the ../routes directory
-const userRoutes = require('../routes/userRoutes');
+//middleware
+//write router middleware here
 
-app.use(express.json());
+//Including product.json file
+const product = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/product.json`)
+);
 
-// Using the userRouter module to handle requests to /api/v1/users
-app.use('/api/v1/users', userRoutes);
+// Defining The Router
+// Get all the products
+router.get('/api/v1/product', (req, res) => {
+  try {
+    //Write your code here
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//Create a new Product
+router.post('/api/v1/product', (req, res) => {
+  try {
+    //Write your code here
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//Registering our Router
+//Write here to register router
 
 module.exports = app;
